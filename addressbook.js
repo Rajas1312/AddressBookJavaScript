0450634af5990537a2fa49e4f72a4ef7e8c9c44a
Rajas1312 <you@example.com>
2021-01-11 19:06:24 +0530

    Added:code to display the person

--- addressbook.js
+++ addressbook.js
@@ -1,16 +1,26 @@
 const fs = require('fs');
 const readLine=require('readline-sync')
-addPerson=()=>{
-    let person = { 
-        firstName: readLine.question("Enter firstname "),
-        lastName: readLine.question("Enter lastname "), 
-        city: readLine.question("Enter city "), 
-        state: readLine.question("Enter state "),  
-        zip: readLine.question("Enter zipcode ")  
-        };
-    let data = JSON.stringify(person,null,2);
-    fs.writeFileSync('person.json', data);
-    }
-    addPerson();
+class AddressBook{
+    addPerson=()=>{
+        let person = { 
+            firstName: readLine.question("Enter firstname "),
+            lastName: readLine.question("Enter lastname "), 
+            city: readLine.question("Enter city "), 
+            state: readLine.question("Enter state "),  
+            zip: readLine.question("Enter zipcode ")  
+            };
+        let data = JSON.stringify(person,null,2);
+        fs.writeFileSync('person.json', data);
+        }
+        displayContacts=()=>{
+            let rawdata = fs.readFileSync('person.json');
+            let person = JSON.parse(rawdata);
+            console.log(person);
+            
+        }
+}
+module.exports=new AddressBook();
+    
+    
     
 
