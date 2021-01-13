72104a7ed08b69ed812dab31fb9cdf96ec3e3d39
Rajas1312 <you@example.com>
2021-01-13 19:16:14 +0530

    Added:ability sort the entries

--- addressbook.js
+++ addressbook.js
@@ -49,7 +49,57 @@ class AddressBook{
                 }
             }
         }
-        
+        sortEntries=()=>{
+            let rawdata = fs.readFileSync('person.json');
+            let person = JSON.parse(rawdata,JSON.stringify(file,null,2));
+            console.log("sort entries 1.firstName 2.city 3.state 4.zip")
+            let options=readLine.question("Enter your option")
+            if(options=="1"){
+                person.sort((a, b)=>{
+                    let x = a.firstName.toLowerCase();
+                    let y = b.firstName.toLowerCase();
+                    if (x < y) {return -1;}
+                    if (x > y) {return 1;}
+                    return 0;
+                });
+                console.log(person)
+                  fs.writeFileSync('person.json',JSON.stringify(person,null,2));
+            }
+            if(options=="2"){
+                person.sort((a, b)=>{
+                    let x = a.city.toLowerCase();
+                    let y = b.city.toLowerCase();
+                    if (x < y) {return -1;}
+                    if (x > y) {return 1;}
+                    return 0;
+                });
+                console.log(person)
+                  fs.writeFileSync('person.json',JSON.stringify(person,null,2));
+            }
+            if(options=="3"){
+                person.sort((a, b)=>{
+                    let x = a.state.toLowerCase();
+                    let y = b.state.toLowerCase();
+                    if (x < y) {return -1;}
+                    if (x > y) {return 1;}
+                    return 0;
+                });
+                console.log(person)
+                  fs.writeFileSync('person.json',JSON.stringify(person,null,2));
+            }
+            if(options=="4"){
+                person.sort((a, b)=>{
+                    let x = a.zip.toLowerCase();
+                    let y = b.zip.toLowerCase();
+                    if (x < y) {return -1;}
+                    if (x > y) {return 1;}
+                    return 0;
+                });
+                console.log(person)
+                  fs.writeFileSync('person.json',JSON.stringify(person,null,2));
+            }
+
+        }   
     }
 module.exports=new AddressBook();
     
